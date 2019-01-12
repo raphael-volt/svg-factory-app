ea1dac452a0ae55e93062b8283f876fd87e54d4a
raphael-volt <raphael@ketmie.com>
2019-01-12 23:41:02 +0100

    export events from change-detection library

--- projects/change-detection/src/public_api.ts
+++ projects/change-detection/src/public_api.ts
@@ -6,3 +6,4 @@ export * from './lib/depth-differ.service';
 export * from './lib/depth-differ.module';
 export * from './lib/core/depth-differ'
 export * from './lib/core/depth-proxy'
+export * from './lib/core/events'
