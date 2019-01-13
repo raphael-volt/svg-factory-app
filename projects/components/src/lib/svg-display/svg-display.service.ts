d5ee7d7bc669ec07ee7e68382949220426a518e1
raphael-volt <raphael@ketmie.com>
2019-01-13 01:18:26 +0100

    code path tranform

--- projects/components/src/lib/svg-display/svg-display.service.ts
+++ projects/components/src/lib/svg-display/svg-display.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
-import { SVGStyle, defaultSVGStyle, defaultSelector } from "./svg-display";
+import {
+  SVGStyle, defaultSVGStyle, defaultSelector,
+  PrintableSymbol, PrintableSymbolConfig, PathTransform, Transform,
+  symbolsSizesProvider, SVGPage
+} from "./svg-display";
+import { PathData, SGMatrix, SGRect, Coord } from "svg-geom";
+import { Margins, px2mm, mm2px } from "tspdf";
 @Injectable({
   providedIn: 'root'
 })
@@ -9,9 +15,103 @@ export class SvgDisplayService {
   rectSelector: string
 
   style: SVGStyle
-  constructor() { 
+  constructor() {
     this.style = defaultSVGStyle()
     this.pathSelector = defaultSelector.path
     this.rectSelector = defaultSelector.rect
+  }
+
+  getTransforms(printables: PrintableSymbol[], page: SVGPage): [PathTransform[], SGRect[]] {
+    const margins: Margins = page.margins
+    const layout: Coord = page.layout
+    const gap: number = page.gap
+    let transforms: Transform[] = this.createTransforms(printables)
+    let pathTransforms: PathTransform[] = []
+    const maxX: number = layout[0] - margins.right
+    const maxY: number = layout[1] - margins.bottom
+
+    let x: number
+    let y: number = margins.top
+    let rowH: number
+    let py: number
+    let pageRect: SGRect
+    let pages: SGRect[] = []
+    let tfm: Transform
+    const createPathTransform = (x: number, y: number, t: Transform) => {
+      pathTransforms.push({
+        m: t.m,
+        p: t.p,
+        tx: x,
+        ty: y
+      })
+    }
+    while (transforms.length) {
+      if (!SGRect) {
+        rowH = 0
+        x = margins.left
+        y = pages.length * (layout[1] + page.bottom)
+        pageRect = new SGRect(0, y, layout[0], layout[1])
+        pages.push(pageRect)
+        py = margins.top
+      }
+      tfm = transforms[0]
+      if (x + tfm.b.width <= maxX) {
+        if (py + tfm.b.height <= maxY) {
+          if (rowH < tfm.b.height)
+            rowH = tfm.b.height
+          transforms.shift()
+          createPathTransform(x, y + py, tfm)
+          x += tfm.b.width + gap
+        }
+        else
+          pageRect = null
+      }
+      else {
+        if (py + tfm.b.height <= maxY) {
+          if (rowH < tfm.b.height)
+            rowH = tfm.b.height
+          py += rowH + gap
+          transforms.shift()
+          x = margins.left
+          createPathTransform(x, y + py, tfm)
+          x += tfm.b.width + gap
+        }
+        else
+          pageRect = null
+      }
+    }
+    return [pathTransforms, pages]
+  }
+  private createTransforms(printables: PrintableSymbol[]) {
+    let transforms: Transform[] = []
+    let p: PathData
+    let bb: SGRect
+    let m: SGMatrix
+    let sx: number
+    let sy: number
+    let h: number
+    for (const printable of printables) {
+      p = new PathData(printable.symbol.data)
+      bb = p.bounds
+      for (const ps of printable.configs) {
+        h = mm2px(symbolsSizesProvider[ps.size])
+        sx = h / bb.height
+        sy = sx
+        if (ps.mirror)
+          sx = - sx
+        for (let i = 0; i < ps.copie; i++) {
+          m = new SGMatrix()
+          m.translate(-(bb.x + bb.width / 2), -(bb.y + bb.height / 2))
+            .scale(sx, sy)
+            .translate(sy * bb.width / 2, sy * bb.height / 2)
+          transforms.push({
+            p: p,
+            m: m,
+            b: new SGRect(0, 0, bb.width * sy, bb.height * sy)
+          })
+        }
+      }
+    }
+    return transforms
   }
 }
