b0a0716a6c761ebda7f21236b3acdacda03eb1a2
raphael-volt <raphael@ketmie.com>
2019-02-06 14:24:00 +0100

    updated catalog config
    - change config interface
    - added draw style editor

--- projects/components/src/lib/catalog/catalog.component.ts
+++ projects/components/src/lib/catalog/catalog.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { DrawStyle } from 'ng-svg/core';
+import { ConfigService, ICatalogConfig } from '../services/config.service';
 
 @Component({
   selector: 'catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css']
+  styleUrls: ['./catalog.component.scss']
 })
 export class CatalogComponent implements OnInit {
 
-  constructor() { }
+  pathStyle: DrawStyle
+  config: ICatalogConfig
+  constructor(
+    private storage: ConfigService
+  ) { 
+    if(storage.catalog)
+      this.setCatalogConfig(storage.catalog)
+    else {
+      const sub = storage.getCatalog().subscribe((config: ICatalogConfig) => {
+        sub.unsubscribe()
+        this.setCatalogConfig(config)
+      })
+    }
+  }
 
+  private setCatalogConfig(config: ICatalogConfig) {
+    this.config = config
+    this.pathStyle = config.style
+  }
+
+  styleChanged(style: DrawStyle) {
+
+  }
   ngOnInit() {
   }
 
