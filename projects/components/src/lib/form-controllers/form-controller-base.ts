f082fa818a0423a595a2c3bb0662272464537e30
raphael-volt <raphael@ketmie.com>
2019-02-13 17:35:43 +0100

    removed ngModel directives

--- projects/components/src/lib/form-controllers/form-controller-base.ts
+++ projects/components/src/lib/form-controllers/form-controller-base.ts
@@ -1,22 +1,24 @@
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Output, EventEmitter, Input, OnDestroy, OnInit, Component } from '@angular/core';
+import { Output, EventEmitter, Input, OnDestroy, OnInit, Component, OnChanges, SimpleChanges } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { callLater } from '../core/call-later';
 import { ErrorController } from '../core/error-controller';
 @Component({
     selector: 'form-controller-base',
-    template:'form-controller-base'
+    template: 'form-controller-base'
 })
-export class FormControllerBase<T> extends ErrorController implements OnDestroy, OnInit {
+export class FormControllerBase<T> extends ErrorController implements OnDestroy, OnInit, OnChanges {
     @Output()
     change: EventEmitter<T> = new EventEmitter<T>()
-
+    @Output()
+    status: EventEmitter<boolean> = new EventEmitter<boolean>()
     @Input()
     data: T
 
+    valid: boolean = false
     formGroup: FormGroup
 
-    private subscription: Subscription
+    private statusSubscription: Subscription
+    private changeSubscription: Subscription
 
     constructor(protected formBuilder: FormBuilder) {
         super()
@@ -28,19 +30,49 @@ export class FormControllerBase<T> extends ErrorController implements OnDestroy,
         this.createFormGroup(this.createControlsConfig())
     }
 
-    protected createFormGroup(controlsConfig: { [key: string]: any }) {
-        const group = this.formBuilder.group(controlsConfig)
-        this.subscription = group.valueChanges.subscribe(change => {
-            if (group.valid) {
-                callLater(() => {
-                    this.change.emit(this.data)
-                })
-            }
-        })
-        this.formGroup = group
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes.data) {
+            this.patch()
+        }
     }
 
+    private patch() {
+        if (this.formGroup) {
+            const data = this.data ? this.data : {}
+            this.formGroup.patchValue(data)
+            this.formGroup.updateValueAndValidity()
+        }
+    }
+
+    protected createFormGroup(controlsConfig: { [key: string]: any }) {
+        const group = this.formBuilder.group(controlsConfig)
+        this.changeSubscription = group.valueChanges.subscribe(value => {
+            if (group.valid) {
+                Object.assign(this.data, value)
+                this.valueChanges(this.data)
+            }
+        })
+        this.statusSubscription = group.statusChanges.subscribe(value => {
+            this.statusChanges(value)
+        })
+        this.formGroup = group
+        this.patch()
+    }
+
+    protected valueChanges(value: T) {
+        this.change.emit(value)
+    }
+    protected statusChanges(value) {
+        const valid = (value == "VALID")
+        if(this.valid != valid) {
+            this.valid = valid
+            this.status.next(valid)
+        }
+    }
+
+
     ngOnDestroy() {
-        this.subscription.unsubscribe()
+        this.statusSubscription.unsubscribe()
+        this.changeSubscription.unsubscribe()
     }
 }
